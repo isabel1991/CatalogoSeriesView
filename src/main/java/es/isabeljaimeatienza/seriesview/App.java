3f0aafa4a1b728640783ca7f8168851d15fc41ec
cadit <cadit@DESKTOP-PCOVD1I>
2020-04-24 13:31:31 +0200

    CatalogoFXView

--- src/main/java/es/isabeljaimeatienza/seriesview/App.java
+++ src/main/java/es/isabeljaimeatienza/seriesview/App.java
@@ -19,17 +19,29 @@ import javax.persistence.Persistence;
 public class App extends Application {
 
     private EntityManagerFactory emf;
-    private EntityManager em;
+    private static EntityManager em;
     private static Scene scene;
-
+    private static PrimaryController primaryController;
+  
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
         emf = Persistence.createEntityManagerFactory("CatalogoSeriesPU");
-        em = emf.createEntityManager();            
+        em = emf.createEntityManager();
+//
+//        scene = new Scene(loadFXML("primary"), 640, 480);
+//        stage.setScene(scene);
+//        stage.show();
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("primary.fxml"));
+    Parent root = fxmlLoader.load();
+    PrimaryController seriesViewController = (PrimaryController) fxmlLoader.getController();                
+    seriesViewController.setEntityManager(em);
+    seriesViewController.cargarTodasSeries();
+    Scene scene = new Scene(root, 300, 250);
+    primaryStage.setTitle("Series");
+    primaryStage.setScene(scene);
+    primaryStage.show();                
 
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+       
 
     }
 
@@ -37,23 +49,26 @@ public class App extends Application {
     public void stop() throws Exception {
         em.close();
         emf.close();
-      
+
         try {
             DriverManager.getConnection("jdbc:derby:CatalogoSeries;shutdown=true");
         } catch (SQLException ex) {
         }
     }
-    
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+       FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+       primaryController=(PrimaryController)fxmlLoader.getController();
+        System.out.println("PrimaryControler:"+ primaryController);
+       // Después de obtener el objeto del controlador y del EntityManager:
+        primaryController.setEntityManager(em);
         return fxmlLoader.load();
+
     }
-    SeriesViewController seriesViewController = (SeriesViewController) fxmlLoader.getController(); 
 
     public static void main(String[] args) {
         launch();
--- src/main/java/es/isabeljaimeatienza/seriesview/PrimaryController.java
+++ src/main/java/es/isabeljaimeatienza/seriesview/PrimaryController.java
@@ -1,12 +1,45 @@
 package es.isabeljaimeatienza.seriesview;
 
-import java.io.IOException;
+import es.isabeljaimeatienza.seriesview.entities.Serie;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable {
+
+
+    private EntityManager entityManager;
+    @FXML
+    private TableView<Serie> tableSeries;
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private TableColumn<Serie, String> columnNombreSerie;
+    @FXML
+    private TableColumn<Serie, String> columnGeneroSerie;
+
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+        
+
     }
+    
+        @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        columnNombreSerie.setCellValueFactory(new PropertyValueFactory<>("título"));
+        columnGeneroSerie.setCellValueFactory(new PropertyValueFactory<>("genero"));
+        
+    }
+    public void cargarTodasSeries() {
+    Query querySerieFindAll = entityManager.createNamedQuery("Serie.findAll");
+    List<Serie> listSerie = querySerieFindAll.getResultList();
+    tableSeries.setItems(FXCollections.observableArrayList(listSerie));
+}  
 }
